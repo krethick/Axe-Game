61bf64faf21331fdfda2a6879511dfc5b0a99267
krethick <krethick@gmail.com>
2024-01-06 00:53:11 +0530

    Just getting started

---
+++ axe_game.cpp
@@ -0,0 +1,6 @@
+// Create the Game, right now we need to create a Popup Window
+// <> They are inbuilt or built in c++
+// " " External Libraries i.e meed to be downloaded seperately.
+
+
+#include "raylib.h"
