99b72fb98f4ab24912337ef6845574fb089dbabe
krethick <krethick@gmail.com>
2024-01-06 01:16:26 +0530

    Intro to data types

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,14 +1,10 @@
-// Create the Game, right now we need to create a Popup Window
-// <> They are inbuilt or built in c++
-// " " External Libraries i.e meed to be downloaded seperately.
-// raylib is not part of the standard c++ library.
-// Functions can have more than one input parameter
-
 #include "raylib.h"
 int main()
 {
-    // Initialise a window
-    // Init window takes a width, height that should be in whole numbers and title should be in "" because it is a string.
-    // To Run just click on start debugging.
-    InitWindow(350,200, "Krethick");
+    // Init window takes a width, height that should be in whole numbers.
+    // Whole numbers are integers and cnnot have a fractional part i.e the number should not have decimal places.
+    // DateType determines how the value is stored in the memory    
+    int width = 350;
+    int height = 200;
+    InitWindow(width,height, "Krethick");
 }
