61a539335f594f13aa1d9ffa5fdc921b300012ce
krethick <krethick@gmail.com>
2024-01-06 01:41:23 +0530

    Insights into float and double data type

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,9 +1,20 @@
 #include "raylib.h"
 int main()
 {
-    // Init window takes a width, height that should be in whole numbers.
-    // Whole numbers are integers and cnnot have a fractional part i.e the number should not have decimal places.
-    // DateType determines how the value is stored in the memory    
+    // We refer fractional number as floating point numbers  
+    // Two Floating data types - Float and Double
+    // Float can take 6-7 digits, Less Precision and requires less memory
+    // Double can take 15-16 digits, More precision and takes more memory
+    // If you want to conserve memory use float or else use double.
+    // Boolean has only True and False, used to make decision in code.
+   
+    float root_beer;
+    root_beer = 1.99;
+    double cheese_burger;
+    cheese_burger = 5.99;
+    bool shouldHaveLunch;
+    shouldHaveLunch = true;
+
     int width = 350;
     int height = 200;
     InitWindow(width,height, "Krethick");
