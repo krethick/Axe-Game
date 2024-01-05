60adef444cd9d2ee55a916ab40fbeb25e01656a1
krethick <krethick@gmail.com>
2024-01-06 01:04:53 +0530

    Inialised a Window

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,6 +1,14 @@
 // Create the Game, right now we need to create a Popup Window
 // <> They are inbuilt or built in c++
 // " " External Libraries i.e meed to be downloaded seperately.
-
+// raylib is not part of the standard c++ library.
+// Functions can have more than one input parameter
 
 #include "raylib.h"
+int main()
+{
+    // Initialise a window
+    // Init window takes a width, height that should be in whole numbers and title should be in "" because it is a string.
+    // To Run just click on start debugging.
+    InitWindow(350,200, "Krethick");
+}
