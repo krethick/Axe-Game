42959de98935857b2c3eec282411a09b6a810112
krethick <krethick@gmail.com>
2024-01-07 00:58:14 +0530

    Completed the concept Debugging

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,19 +1,31 @@
 #include "raylib.h"
 int main()
 {
-    // We refer fractional number as floating point numbers  
-    // Two Floating data types - Float and Double
-    // Float can take 6-7 digits, Less Precision and requires less memory
-    // Double can take 15-16 digits, More precision and takes more memory
-    // If you want to conserve memory use float or else use double.
-    // Boolean has only True and False, used to make decision in code.
-   
-    float root_beer;
-    root_beer = 1.99;
-    double cheese_burger;
-    cheese_burger = 5.99;
-    bool shouldHaveLunch;
-    shouldHaveLunch = true;
+   // Debugging is the process of finding and removing errors.
+   // The dark red dot is the break point 
+   // We can continue to the next line by clicking the step into.
+   // We can use the continue or f5 to move towards the next breakpoint.
+   // Has Garbage data and ways to initialize.
+
+   /*
+     Initialization by Assignment
+     Example:
+
+     float root_beer;     UNINITIALIZED
+     root_beer = 1.99;
+ 
+     float root_beer = 1.99 INITIALIZED
+
+     Braced Initialization
+
+     double cheese_burger{5.99}; INITIALIZED
+     double cheese_burger{};  INITIALIZED TO 0
+     
+   */
+
+    float root_beer = 1.99;
+    double cheese_burger{5.99};
+    bool shouldHaveLuch{};
 
     int width = 350;
     int height = 200;
