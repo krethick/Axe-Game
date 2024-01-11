70ef20c780b5835e5078fdae4c0e433d1b2fd641
krethick <krethick@gmail.com>
2024-01-11 19:38:14 +0530

    Implemented the Close Window Functionality

--- axe_game.cpp
+++ axe_game.cpp
@@ -3,20 +3,33 @@
 using namespace std;
 int main()
 {
-  // Clear the window background using raylib colors.
-  // FrameBuffer causes change in between color white and black in the window.
-  // The outer portion is the window and the inner portion is the canvas area.
+   // We are going to see how functions are going to return values or outputs
+   // Implement close window functionality
+   /*
+    How to return a function in 3 ways
 
+    -> int answerToLife = getTheAnswer();
+    -> int theUniverse { getTheAnswer()};
+    -> useAnswer (getTheAnswer));
+
+    -> WindowShouldClose() => Closes the window if x icon clicked or escape button is pressed
+    -> Two ways to use it in while condition
+      i) WindowShouldClose() != true
+     ii) WindowShouldClose() == false
+   */ 
    int width;
+   
    // Set the width size
    width = 350;
    // Initialise the window
    InitWindow(width,200, "Axe Game");
-   while(true)
+   while (WindowShouldClose() != true) // Until this action execued, the screen stays open. 
    {
     BeginDrawing(); // AIDS IN SETUP AND TEARDOWN OF THE CANVAS (Does the heavy lifting)
     ClearBackground(RED); // We need to clear the BG to avoid flickering.
     EndDrawing(); // AIDS IN SETUP AND TEARDOWN OF THE CANVAS (Does the heavy lifting)
    }
+
+ 
     
 }
