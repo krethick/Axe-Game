f00d60c88ededf30404903372a59a25d47c6aae8
krethick <krethick@gmail.com>
2024-01-11 16:09:23 +0530

    Completed the cocnept of loops

--- axe_game.cpp
+++ axe_game.cpp
@@ -3,26 +3,12 @@
 using namespace std;
 int main()
 {
-    // Keep the Window Open
-    // Loop means repeating a code until the condition is met.
-    // We call this loop condition as Boolean Condition
-    // Loop executes if true
-    /*
-      expression -> condition -> true -> Statement (Body)
-      again goes back to the expression until the condition is false.
-    */
-     
-   // Initial value of milesToDisneyland is 100;
-   int milesToDisneyland {100};
-   // Afer that the loop is implemented.
-   // Eg milesToDisneyland 100 > 0
-   while(milesToDisneyland > 0 )
+   int width;
+   width = 350;
+   InitWindow(width,200, "Axe Game");
+   while(true)
    {
-    // If True the statement gets executed.
-    // After that 100 gets decremented to 1.
-    // This process continues until milesToDisneyland becomes 0.
-    cout<<" \n Are we there yet";
-    milesToDisneyland = milesToDisneyland - 1;
+
    }
     
 }
