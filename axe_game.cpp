491c8e144711b57a358484c6073e19e59f22e1b9
krethick <krethick@gmail.com>
2024-01-12 00:18:44 +0530

    Completed making a circle

--- axe_game.cpp
+++ axe_game.cpp
@@ -3,30 +3,29 @@
 using namespace std;
 int main()
 {
-   // We are going to see how functions are going to return values or outputs
-   // Implement close window functionality
+   
+   // Draw a Circle
    /*
-    How to return a function in 3 ways
-
-    -> int answerToLife = getTheAnswer();
-    -> int theUniverse { getTheAnswer()};
-    -> useAnswer (getTheAnswer));
-
-    -> WindowShouldClose() => Closes the window if x icon clicked or escape button is pressed
-    -> Two ways to use it in while condition
-      i) WindowShouldClose() != true
-     ii) WindowShouldClose() == false
+    i)Raylib has a function called Draw Circle.
+    ii)Draw Circle takes three inputs centerX, centerY and radius.
+    the color input takes it as raylib color.
+    Radius is half of a cricle's diameter
+   iii) The centerX and centerY are X and Y coordinates stored in pixles.
+    iv) We used half of the windows width and half of the windows height to place the 
+        cirlce in the center.
    */ 
    int width;
-   
+   int height;
    // Set the width size
    width = 350;
+   height = 200;
    // Initialise the window
-   InitWindow(width,200, "Axe Game");
+   InitWindow(width,height, "Axe Game");
    while (WindowShouldClose() != true) // Until this action execued, the screen stays open. 
    {
     BeginDrawing(); // AIDS IN SETUP AND TEARDOWN OF THE CANVAS (Does the heavy lifting)
-    ClearBackground(RED); // We need to clear the BG to avoid flickering.
+    ClearBackground(WHITE); // We need to clear the BG to avoid flickering.
+    DrawCircle(175,100,25,BLUE);
     EndDrawing(); // AIDS IN SETUP AND TEARDOWN OF THE CANVAS (Does the heavy lifting)
    }
 
