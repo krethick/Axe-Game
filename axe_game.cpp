d50d021ab70ee20b05e69e85db10a0b21c84a81f
krethick <krethick@gmail.com>
2024-01-13 08:33:35 +0530

    Completed drawing the rectangle(Axe)

--- axe_game.cpp
+++ axe_game.cpp
@@ -4,22 +4,36 @@ using namespace std;
 int main()
 {
    /*
-    LOGICAL AND OPERATOR
-      * To check if both of two conditions are true. 
+    DRAWING THE AXE
 
-    circle_x < 350 is the extreme right as width is the x-axis
-    circle_x > 0 is the extreme left as width is the x-axis
-   */
+    DrawRectangle(posX, posY, height, color)
+     -> posX, posY, width and height are ints.
+     -> color is a raylib color
+     -> posX and posY are the upper left corner
+
+     Compound Assignment Operator
+     -> axe_y = axe_y + 10; Normal Operator
+     -> axe_y += 10; Compound Assignment Operator
+   */ 
 
    // Window Dimensions
-   int width{350}; // Use braced initialisation and set the width size
-   int height{200}; // Use braced initialisation and set the height size
+   int width{800}; // Use braced initialisation and set the width size
+   int height{450}; // Use braced initialisation and set the height size
+
+   // Rectangle Dimensions
+   int rectangle_width{50};
+   int rectangle_height{50};
+
    // Initialise the window
    InitWindow(width,height, "Axe Game");
 
    // Circle coordinates
-   int circle_x{175};
-   int circle_y{100};
+   int circle_x{200};
+   int circle_y{200};
+
+   // Axe(Rectangle) coordinates
+   int axe_x{400};
+   int axe_y{0};
    
    // SetTargetFPS is used to control the framerate at which your game updates and renders
    SetTargetFPS(60);
@@ -31,15 +45,19 @@ int main()
     // This is where game logic starts
 
     DrawCircle(circle_x,circle_y,25,BLUE);
+    DrawRectangle(axe_x,axe_y,rectangle_width,rectangle_height,RED);
+
+    // Move the axe towards y-axis or downwards
+    axe_y += 10; // Compound Assignment Operator
     
     if(IsKeyDown(KEY_D) && circle_x < 350) // circle_x < 350 (it is the extreme right) sets a boundary where the circle can't cross even further.
     {
-        circle_x = circle_x + 10;
+        circle_x += 10; // Compound Assignment Operator
     }
 
     if(IsKeyDown(KEY_A) && circle_x > 0)// circle_y > 0 (it is the extreme left). sets a boundary where the circle can't cross even further.
     {
-        circle_x = circle_x - 10;
+        circle_x -= 10; // Compound Assignment Operator
     }
 
     // This is where game logic ends
