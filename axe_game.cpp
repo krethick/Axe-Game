bbd51bb64d9fb2bac98f45f71073f063d154c822
krethick <krethick@gmail.com>
2024-01-13 23:49:26 +0530

    Completed the else statement

--- axe_game.cpp
+++ axe_game.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
    /*
-    Collision
-      -> For eg circle_x = 225 and Radius = 25
-       Then the total would be circle_x + R = 225
-       circle_x + R >= 300
+    The else statement
 
-      -> circle_x - R <= 350
-
+    DrawText is a raylib function
+    ->DrawText(text, posx, posY, fontSize, color);
+    ->Text is the text in double quotes
+    ->posX, posY are the coordinates of the text
+    ->fontSize is the size of the font.
 
    */ 
 
@@ -42,6 +42,8 @@ int main()
    int r_axe_x{axe_x + axe_length};
    int u_axe_y{axe_y};
    int b_axe_y{axe_y + axe_length};
+   
+   bool collision_with_axe{false}; // Initialise the value with true
 
    int direction{10};
 
@@ -53,34 +55,41 @@ int main()
    {
     BeginDrawing(); // AIDS IN SETUP AND TEARDOWN OF THE CANVAS (Does the heavy lifting)
     ClearBackground(WHITE); // We need to clear the BG to avoid flickering.
-
-    // This is where game logic starts
-
-    DrawCircle(circle_x,circle_y,circle_radius,BLUE);
-    DrawRectangle(axe_x,axe_y,axe_length,axe_length,RED);
-
-    // Move the axe towards y-axis or downwards
-    axe_y += direction; // Compound Assignment Operator
-
-    // 450 is the height of the screen window
-    if(axe_y > height || axe_y < 0)
-    {
-      direction = -direction; // negates the current value 
-    }
     
-    if(IsKeyDown(KEY_D) && circle_x < width) // circle_x < 350 (it is the extreme right) sets a boundary where the circle can't cross even further.
-    {
-        circle_x += 10; // Compound Assignment Operator
-    }
+      if(collision_with_axe == true)
+      {
+        DrawText("Game Over!",400,200,20,RED);
+      }
+      else
+      {
+      // This is where game logic starts
+    
+      DrawCircle(circle_x,circle_y,circle_radius,BLUE);
+      DrawRectangle(axe_x,axe_y,axe_length,axe_length,RED);
 
-    if(IsKeyDown(KEY_A) && circle_x > 0)// circle_y > 0 (it is the extreme left). sets a boundary where the circle can't cross even further.
-    {
-        circle_x -= 10; // Compound Assignment Operator
-    }
+      // Move the axe towards y-axis or downwards
+      axe_y += direction; // Compound Assignment Operator
 
-    // This is where game logic ends
+      // 450 is the height of the screen window
+      if(axe_y > height || axe_y < 0)
+      {
+        direction = -direction; // negates the current value 
+      }
+      
+      if(IsKeyDown(KEY_D) && circle_x < width) // circle_x < 350 (it is the extreme right) sets a boundary where the circle can't cross even further.
+      {
+          circle_x += 10; // Compound Assignment Operator
+      }
+
+      if(IsKeyDown(KEY_A) && circle_x > 0)// circle_y > 0 (it is the extreme left). sets a boundary where the circle can't cross even further.
+      {
+          circle_x -= 10; // Compound Assignment Operator
+      }
+
+      // This is where game logic ends
+  }
     EndDrawing(); // AIDS IN SETUP AND TEARDOWN OF THE CANVAS (Does the heavy lifting)
-   }
+  }
 }
  
     
