a68e33514b9d965bd41c67507a91f8f4ab50ac7d
krethick <krethick@gmail.com>
2024-01-14 00:41:00 +0530

    Completed the game and Understood the collision concept in detail

--- axe_game.cpp
+++ axe_game.cpp
@@ -4,14 +4,7 @@ using namespace std;
 int main()
 {
    /*
-    The else statement
-
-    DrawText is a raylib function
-    ->DrawText(text, posx, posY, fontSize, color);
-    ->Text is the text in double quotes
-    ->posX, posY are the coordinates of the text
-    ->fontSize is the size of the font.
-
+    Collision detection involves checking the edges of a box to see if it is overlapping with the other box.
    */ 
 
    // Window Dimensions
@@ -26,7 +19,7 @@ int main()
    int circle_y{200};
    int circle_radius{25};
    
-   // Circle edges
+   // Circle edges, edges of the x and y axis of the circle
    int l_circle_x{circle_x - circle_radius}; // Left side
    int r_circle_x{circle_x + circle_radius}; // Right side
    int u_circle_y{circle_y - circle_radius}; // upper edge
@@ -37,13 +30,18 @@ int main()
    int axe_y{0};
    int axe_length{50}; // We will be using it for width and height.
    
-   // axe edges
+   // axe edges. edges of the x and y axis of the axe
    int l_axe_x{axe_x};
    int r_axe_x{axe_x + axe_length};
    int u_axe_y{axe_y};
    int b_axe_y{axe_y + axe_length};
    
-   bool collision_with_axe{false}; // Initialise the value with true
+   bool collision_with_axe=
+                 (b_axe_y >= u_circle_y) && 
+                 (u_axe_y <= b_circle_y) && 
+                 (r_axe_x >= l_circle_x) && 
+                 (l_axe_x <= r_circle_x); 
+                 // b for bottom, u for upper, l for left and r for right
 
    int direction{10};
 
@@ -56,14 +54,32 @@ int main()
     BeginDrawing(); // AIDS IN SETUP AND TEARDOWN OF THE CANVAS (Does the heavy lifting)
     ClearBackground(WHITE); // We need to clear the BG to avoid flickering.
     
-      if(collision_with_axe == true)
+      if(collision_with_axe)
       {
         DrawText("Game Over!",400,200,20,RED);
       }
       else
       {
       // This is where game logic starts
-    
+      
+      // Update the edges for the circle and rectangle
+      l_circle_x = circle_x - circle_radius;
+      r_circle_x = circle_x + circle_radius;
+      u_circle_y = circle_y - circle_radius;
+      b_circle_y = circle_y + circle_radius;
+      
+      l_axe_x = axe_x;
+      r_axe_x = axe_x + axe_length;
+      u_axe_y = axe_y;
+      b_axe_y = axe_y + axe_length;
+
+      // Update collision_with_axe
+      collision_with_axe=
+                 (b_axe_y >= u_circle_y) && 
+                 (u_axe_y <= b_circle_y) && 
+                 (r_axe_x >= l_circle_x) && 
+                 (l_axe_x <= r_circle_x); 
+
       DrawCircle(circle_x,circle_y,circle_radius,BLUE);
       DrawRectangle(axe_x,axe_y,axe_length,axe_length,RED);
 
